2a74ed24fa4718219f03d0b82f6277a47b0d1770
Kyle Simmons <kyle.simmons@consilio.com>
2024-08-13 18:56:04 +0200

    add text files and create grid method

--- ConnectedShapesCounter/Program.cs
+++ ConnectedShapesCounter/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string fileName = "data_small.txt"; //data_large.txt
+        char[][] grid = ReadGridFromFile(fileName);
+    }
+
+    static char[][] ReadGridFromFile(string filename)
+    {
+        string[] lines = File.ReadAllLines(filename);
+        char[][] grid = new char[lines.Length][];
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            grid[i] = lines[i].ToCharArray();
+        }
+
+        return grid;
+    }
+}
