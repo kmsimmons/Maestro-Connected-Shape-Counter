bcc094681deff772178019b29e2048f701088665
Kyle Simmons <kyle.simmons@consilio.com>
2024-08-13 19:42:43 +0200

    add implementation to count shapes

--- ConnectedShapesCounter/Program.cs
+++ ConnectedShapesCounter/Program.cs
@@ -1,12 +1,20 @@
-﻿class Program
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
 {
     static void Main(string[] args)
     {
         string fileName = "data_small.txt"; //data_large.txt
-        char[][] grid = ReadGridFromFile(fileName);
+
+        // read the grid and create a jagged array
+        char[][] grid = CreateGridFromFile(fileName);
+        int result = CountConnectedShapes(grid);
+        Console.WriteLine($"Number of connected shapes in {fileName}: {result}");
     }
 
-    static char[][] ReadGridFromFile(string filename)
+    static char[][] CreateGridFromFile(string filename)
     {
         string[] lines = File.ReadAllLines(filename);
         char[][] grid = new char[lines.Length][];
@@ -17,5 +25,65 @@
         }
 
         return grid;
+    }
+
+    static int CountConnectedShapes(char[][] grid)
+    {
+        // number of rows in the grid
+        int rowCount = grid.Length;
+
+        // number of columns in the first row of the grid
+        int columnCount = grid[0].Length;
+
+        // track visited cells -> init same size as grid
+        bool[][] visited = new bool[rowCount][];
+        int shapeCount = 0;
+
+        for (int i = 0; i < rowCount; i++)
+        {
+            visited[i] = new bool[columnCount];
+        }
+
+        for (int row = 0; row < rowCount;row++)
+        {
+            for (int column = 0; column < columnCount;column++)
+            {
+                if (grid[row][column] == '1' && !visited[row][column])
+                {
+                    // perform a Depth-First-Search for a new shape
+                    DepthFirstSearch(grid, visited, row, column);
+                    shapeCount++;
+                }
+            }
+        }
+        return shapeCount;
+    }
+
+    static void DepthFirstSearch(char[][] grid, bool[][] visited, int row, int column)
+    {
+        int[] dRow = { -1, 1, 0, 0 };
+        int[] dColumn = { 0, 0, -1, 1 };
+
+        // mark current cell as visited
+        visited[row][column] = true;
+
+        // check adjacent cells
+        for (int i = 0; i < 4; i++)
+        {
+            int newRow = row + dRow[i];
+            int newColumn = column + dColumn[i];
+            if (IsAdjacent(grid, visited, newRow, newColumn))
+            {
+                DepthFirstSearch(grid, visited, newRow, newColumn);
+            }
+        }
+    }
+
+    static bool IsAdjacent(char[][] grid, bool[][] visited, int row, int column)
+    {
+        return row >= 0 && row < grid.Length &&
+            column >= 0 && column < grid[0].Length &&
+            grid[row][column] == '1' &&
+            !visited[row][column];
     }
 }
